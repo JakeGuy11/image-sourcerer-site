98fe9e68b46a8a2451c30dba684e35cf29aedc1b
Jake <Jake_Guy_11@protonmail.ch>
2021-08-27 22:11:27 -0400

    added links to other pages

--- header.js
+++ header.js
@@ -1,11 +1,11 @@
 let content = `
 <ul>
-    <li style="padding:0px;float:left;"><img style="height:100px;margin:10px;" src="res/logo.svg"></li>
-    <li><a class="nav-entry" href="https://www.google.com/"><img height=32px src="res/menu-options/download.svg"></a></li>
-    <li><a class="nav-entry" href="https://www.google.com/"><img height=32px src="res/menu-options/contribute.svg"></a></li>
-    <li><a class="nav-entry" href="https://www.google.com/"><img height=32px src="res/menu-options/about.svg"></a></li>
-    <li><a class="nav-entry" href="https://www.google.com/"><img height=42px style="margin-top:10px;" src="res/menu-options/help.svg"></a></li>
-    <li><a class="nav-entry" href="https://www.google.com/"><img height=32px src="res/menu-options/decode.svg"></a></li>
+    <li style="padding:0px;float:left;"><a href="index.html"><img style="height:100px;margin:10px;" src="res/logo.svg"></a></li>
+    <li><a class="nav-entry" href="download.html"><img height=32px src="res/menu-options/download.svg"></a></li>
+    <li><a class="nav-entry" href="contribute.html"><img height=32px src="res/menu-options/contribute.svg"></a></li>
+    <li><a class="nav-entry" href="about.html"><img height=32px src="res/menu-options/about.svg"></a></li>
+    <li><a class="nav-entry" href="help.html"><img height=42px style="margin-top:10px;" src="res/menu-options/help.svg"></a></li>
+    <li><a class="nav-entry" href="decode.html"><img height=32px src="res/menu-options/decode.svg"></a></li>
 </ul>
 `;
 document.body.innerHTML = content + document.body.innerHTML;
