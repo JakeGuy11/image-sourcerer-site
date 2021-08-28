37dc9e124610a38a1e9c4619ff7d962e1b3656e4
Jake <Jake_Guy_11@protonmail.ch>
2021-08-27 22:04:48 -0400

    set labels to images

--- header.js
+++ header.js
@@ -1,10 +1,11 @@
 let content = `
 <ul>
-    <li style="padding:0px;float:left;"><img style="height:100px;margin:10px;" src="res/icons/logo-large.png"></li>
-    <li><a class="nav-entry" href="https://www.google.com/">Item1</a></li>
-    <li><a class="nav-entry" href="https://www.google.com/">Item2</a></li>
-    <li><a class="nav-entry" href="https://www.google.com/">Item3</a></li>
-    <li><a class="nav-entry" href="https://www.google.com/">Item4</a></li>
+    <li style="padding:0px;float:left;"><img style="height:100px;margin:10px;" src="res/logo.svg"></li>
+    <li><a class="nav-entry" href="https://www.google.com/"><img height=32px src="res/menu-options/download.svg"></a></li>
+    <li><a class="nav-entry" href="https://www.google.com/"><img height=32px src="res/menu-options/contribute.svg"></a></li>
+    <li><a class="nav-entry" href="https://www.google.com/"><img height=32px src="res/menu-options/about.svg"></a></li>
+    <li><a class="nav-entry" href="https://www.google.com/"><img height=42px style="margin-top:10px;" src="res/menu-options/help.svg"></a></li>
+    <li><a class="nav-entry" href="https://www.google.com/"><img height=32px src="res/menu-options/decode.svg"></a></li>
 </ul>
 `;
 document.body.innerHTML = content + document.body.innerHTML;
