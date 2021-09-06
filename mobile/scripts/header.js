c5ac3bd148ab86725882944e103894d3642a6dd6
Jake <Jake_Guy_11@protonmail.ch>
2021-09-05 23:01:02 -0400

    added entries to mobile menu

--- mobile/scripts/header.js
+++ mobile/scripts/header.js
@@ -3,8 +3,17 @@ let header_content = `
     <li style="padding:0px;float:left;"><a href="index.html"><img style="height:100px;margin:10px;" src="res/logo.svg"></a></li>
     <li class="header"><div style="height:120px;width:120px;display:table-cell;vertical-align:middle;text-align:center;"><img id="show-menu" height=42px src="res/menu.svg"></div></li>
 </ul>
-<div id="dropdown-menu" style="display:none;min-width:100px;">
+<div id="dropdown-menu" style="opacity:0;visibility:hidden;">
     <img id="close-menu" height=42px style="display:inline-block;top:5px;float:right;" src="res/close-button.svg">
+    <div style="display:block;margin-right:45px;">
+        <center>
+            <img style="padding:15px;height:32px;display:block;" src="res/menu-options/decode.svg">
+            <img style="padding:15px;height:42px;display:block;" src="res/menu-options/help.svg">
+            <img style="padding:15px;height:32px;display:block;" src="res/menu-options/about.svg">
+            <img style="padding:15px;height:32px;display:block;" src="res/menu-options/contribute.svg">
+            <img style="padding:15px;height:32px;display:block;" src="res/menu-options/download.svg">
+        </center>
+    </div>
 </div>
 `;
 
@@ -17,10 +26,12 @@ let footer_content = `
 document.body.innerHTML = header_content + document.body.innerHTML + footer_content;
 
 document.getElementById("show-menu").addEventListener("click", function() {
-    document.getElementById("dropdown-menu").style.display = "block";
+    document.getElementById("dropdown-menu").style.opacity = "1";
+    document.getElementById("dropdown-menu").style.visibility = "visible";
 });
 
 document.getElementById("close-menu").addEventListener("click", function() {
-    document.getElementById("dropdown-menu").style.display = "none";
+    document.getElementById("dropdown-menu").style.opacity = "0";
+    document.getElementById("dropdown-menu").style.visibility = "hidden";
 });
 
