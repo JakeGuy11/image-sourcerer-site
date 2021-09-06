9144234510292996844643e601b0891d81aad10b
Jake <Jake_Guy_11@protonmail.ch>
2021-09-05 20:58:16 -0400

    added basic working dropdown menu

--- mobile/scripts/header.js
+++ mobile/scripts/header.js
@@ -1,8 +1,11 @@
 let header_content = `
 <ul class="header">
     <li style="padding:0px;float:left;"><a href="index.html"><img style="height:100px;margin:10px;" src="res/logo.svg"></a></li>
-    <li class="header"><a class="nav-entry" href="decode.html"><img height=32px src="res/menu-options/decode.svg"></a></li>
+    <li class="header"><div style="height:120px;width:120px;display:table-cell;vertical-align:middle;text-align:center;"><img id="show-menu" height=42px src="res/menu.svg"></div></li>
 </ul>
+<div id="dropdown-menu" style="display:none;min-width:100px;">
+    <img id="close-menu" height=42px style="display:inline-block;top:5px;float:right;" src="res/close-button.svg">
+</div>
 `;
 
 let footer_content = `
@@ -12,3 +15,12 @@ let footer_content = `
 `;
 
 document.body.innerHTML = header_content + document.body.innerHTML + footer_content;
+
+document.getElementById("show-menu").addEventListener("click", function() {
+    document.getElementById("dropdown-menu").style.display = "block";
+});
+
+document.getElementById("close-menu").addEventListener("click", function() {
+    document.getElementById("dropdown-menu").style.display = "none";
+});
+
